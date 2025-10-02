142fe302403c67a95975ca9c3e3265495711d20e
Your Name <you@example.com>
2025-10-02 00:29:52 -0400

    Search todo

--- travel_rec.js
+++ travel_rec.js
@@ -3,25 +3,27 @@
 const btnSearch = document.getElementById('btnSearch');
 const rec = [];
 
-function resetForm() {
-      document.getElementById("name").value = "";
-      document.querySelector('input[name="gender"]:checked').checked = false;
-      document.getElementById("age").value = "";
-      document.getElementById("condition").value = "";
+function resetSearch() {
+      document.getElementById("searchInput").placeholder = "Enter 'beach', 'temple', or 'country";
+      document.getElementById("s1-i").src = "";
+      document.getElementById("s1-h2").textContent = "";
+      document.getElementById("s1-p").textContent = "";
+      document.getElementById("s2-i").src = "";
+      document.getElementById("s2-h2").textContent = "";
+      document.getElementById("s2-p").textContent = "";
     }    
 
+btnSearch.addEventListener('click', resetSearch);
 
-function searchCondition() {
-    const input = document.getElementById('conditionInput').value.toLowerCase();
-    const resultDiv = document.getElementById('result');
-    resultDiv.innerHTML = '';
+function search() {
+    const input = document.getElementById('searchInput').value.toLowerCase();
 
     fetch('./travel_rec.json')
       .then(response => response.json())
       .then(data => {
-        const condition = data.conditions.find(item => item.name.toLowerCase() === input);
 
-        if (condition) {
+        if (input == "country") {
+          console.log("Recommendation found!");
           const symptoms = condition.symptoms.join(', ');
           const prevention = condition.prevention.join(', ');
           const treatment = condition.treatment;
@@ -32,13 +34,18 @@ function searchCondition() {
           resultDiv.innerHTML += `<p><strong>Symptoms:</strong> ${symptoms}</p>`;
           resultDiv.innerHTML += `<p><strong>Prevention:</strong> ${prevention}</p>`;
           resultDiv.innerHTML += `<p><strong>Treatment:</strong> ${treatment}</p>`;
+        } else if (input == "temple"){
+
+        } else if (input == "beach"){
+
         } else {
-          resultDiv.innerHTML = 'Condition not found.';
+            console.log("NO Recommendation found!");
         }
       })
       .catch(error => {
         console.error('Error:', error);
-        resultDiv.innerHTML = 'An error occurred while fetching data.';
+        console.log("NO Recommendation found!");
       });
   }
-    btnSearch.addEventListener('click', searchCondition);
+
+  btnSearch.addEventListener('click', btnSearch);
